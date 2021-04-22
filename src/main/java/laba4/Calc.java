57404a622460f54f2070a8415f39a523da695f56
demenev26 <demenev_konstantin_01@mail.ru>
2021-04-22 23:56:23 +0500

    Изменил код в Calc.java

--- src/main/java/laba4/Calc.java
+++ src/main/java/laba4/Calc.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="Calc", urlPatterns="/JavaCalc") //связывание сервлета с URL
+@WebServlet(name="Calc", urlPatterns="/JavaCalc") //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ URL
 public class Calc extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,7 +22,8 @@ public class Calc extends HttpServlet {
 	private static class RequestCalc {
 		private final String first_calc;
 		private final String second_calc;
-		private int result;
+		private double result;
+		private double result_in_mm;
 						
 		private RequestCalc (String first, String second) {
 			this.first_calc = first;
@@ -38,19 +39,23 @@ public class Calc extends HttpServlet {
 		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
 			request.setAttribute("first_result", first_calc);
 			request.setAttribute("second_result", second_calc);
-			int first_try;
-			int second_try;
+			double first_try;
+			double second_try;
 			try { 
-			first_try=Integer.parseInt(first_calc);
-			second_try=Integer.parseInt(second_calc);
+			first_try=Double.parseDouble(first_calc);
+			second_try=Double.parseDouble(second_calc);
 			}
 			catch (NumberFormatException e) {
 				first_try=0;
 				second_try=0;	
 			}
 			
-			result=first_try+second_try;
-			request.setAttribute("result", result);
+			result=2 * Math.PI * first_try * first_try * second_try / 3;
+			String formattedResult = String.format("%.2f",result);
+			result_in_mm = result * 1000000;
+			String formattedResultInMm = String.format("%.2f",result_in_mm);
+			request.setAttribute("formattedResult", formattedResult);
+			request.setAttribute("formattedResultInMm", formattedResultInMm);
 		}
 		
 	}
